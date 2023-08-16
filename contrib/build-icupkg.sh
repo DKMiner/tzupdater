7b5ddd35233d43e427998f537abe40076b8f6f79
DKMiner <mohamadrezadarqazvin@gmail.com>
2023-08-16 17:21:39 +0330

    deleting original repo

--- contrib/build-icupkg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget -O - http://download.icu-project.org/files/icu4c/63.1/icu4c-63_1-src.tgz | tar xz
-cd icu/source
-./configure --enable-static --disable-shared
-make LDFLAGS="-static"
-strip -s bin/icupkg
--- contrib/build-zic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir tzcode
-wget -O - https://data.iana.org/time-zones/tzcode-latest.tar.gz | tar xz -C tzcode
-cd tzcode
-make version.h
-gcc zic.c -o zic -static
-strip -s zic
